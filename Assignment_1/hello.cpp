a47ffff5815d2f3863ea142190802f1cf4008275
Artem <st142163@student.spbu.ru>
2025-12-12 23:34:11 +0300

    test astyle

--- Assignment_1/hello.cpp
+++ Assignment_1/hello.cpp
@@ -9,13 +9,16 @@ My project number one
 #include <iostream>
 #include <string>
 
-void myproject::sayHello() {
-  std::string word = "world";
-  while (true) {
-    std::cout << "Hello, " << word << "!" << std::endl;
-    std::getline(std::cin, word);
-    if (word.empty()) {
-      break;
+void myproject::sayHello()
+{
+    std::string word = "world";
+    while (true)
+    {
+        std::cout << "Hello, " << word << "!" << std::endl;
+        std::getline(std::cin, word);
+        if (word.empty())
+        {
+            break;
+        }
     }
-  }
 }
